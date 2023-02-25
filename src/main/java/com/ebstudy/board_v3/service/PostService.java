4abd73832759819053ca027c06289e6a70c51c99
thddntjr1234 <thddntjr112233@gmail.com>
2023-02-26 01:43:04 +0900

    Feat: 게시글 서비스에서 유틸 메소드 분리 및 주석 추가(리팩토링 예정)

--- src/main/java/com/ebstudy/board_v3/service/PostService.java
+++ src/main/java/com/ebstudy/board_v3/service/PostService.java
@@ -3,15 +3,14 @@ package com.ebstudy.board_v3.service;
 import com.ebstudy.board_v3.dto.CategoryDTO;
 import com.ebstudy.board_v3.dto.PostDTO;
 import com.ebstudy.board_v3.repository.BoardMapper;
+import com.ebstudy.board_v3.util.PostServiceUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -20,25 +19,30 @@ import java.util.List;
 public class PostService {
 
     private final BoardMapper boardMapper;
+    private final PostServiceUtil postServiceUtil;
 
     /**
-     *
-     * @param pageNumber
-     * @return
+     * 요구 데이터 포맷에 맞게 변환된 게시글 리스트를 가져오는 메소드
+     * @param pageNumber 예외처리 없이 입력된 페이지 값
+     * @return viewName과 데이터를 포함안 ModelAndView 객체
      */
     public ModelAndView getPostList(Integer pageNumber) {
 
-        if (pageNumber == null || pageNumber == 0) {
-            pageNumber = 1;
-        }
-
         int totalPostCount = boardMapper.getPostCount();
 
-        HashMap<String, Integer> pagingValues = calPagingValues(totalPostCount, pageNumber);
+        // TODO: 2/25 Map으로 리턴하는 게 아니라 PagenationDTO와 같이 객체 형식으로 해줘야 안에 데이터가 무슨 값이 들었는지 유추할 수 있다
+        HashMap<String, Integer> pagingValues = postServiceUtil.calPagingValues(totalPostCount, pageNumber);
         List<CategoryDTO> categoryList = boardMapper.getCategoryList();
+
         // 가져올 게시글의 개수를 지정, ex) 15개의 게시글이 있다면 1페이지는 10개, 2페이지는 5개를 가져오도록 함
-        int maximumPostCount = (pageNumber - 1) * 10;
-        List<PostDTO> postList = convertPostListData(boardMapper.getPostList(maximumPostCount));
+        int startPostNumber = pagingValues.get("startPostNumber");
+        //List<PostDTO> postList = postServiceUtil.convertPostListData(boardMapper.getPostList(startPostNumber));
+        // TODO: postList를 단건으로 convert하도록 메소드를 수정
+        List<PostDTO> postList = postServiceUtil.convertPostListData(boardMapper.getPostList(startPostNumber));
+        List<PostDTO> sourcePostList = boardMapper.getPostList(startPostNumber);
+//        sourcePostList.forEach((post)=>{
+//                postServiceUtil.convertPostListData(post);
+//        });
 
         // 페이징에 필요한 변수와 카테고리 리스트, 총 게시글 개수와 페이징된 게시글 리스트를 ModelAndView 객체에 저장
         ModelAndView mv = new ModelAndView("list");
@@ -52,124 +56,43 @@ public class PostService {
     }
 
     /**
-     *
-     * @param postId
-     * @return 게시글 데이터와
+     * 게시글 불러오기 메소드
+     * postId 파라미터에 대한 유효성 검증과 게시글 로딩을 수행
+     * @param postId 게시글 id
+     * @return 게시글 데이터와 viewName을 가진 ModelAndView 객체 반환
      */
-    public ModelAndView getPost(Long postId) {
-
-        // TODO: 로그 기능 별도로 사용하기
-
-        ModelAndView mv = new ModelAndView();
-
-        // null, 0, ""은 list 페이지로 리다이렉트
-        if ("0".equals(postId) || "".equals(postId) || postId == null) {
-            mv.setViewName("redirect:/boards/free/list");
-            log.info("파라미터 postId값이 유효하지 않아 /list 리다이렉트됨");
-            return mv;
-        }
-
-        List<CategoryDTO> categoryList = boardMapper.getCategoryList();
-
+    public PostDTO getPost(Long postId) {
         // 게시글 로드
-        boardMapper.increaseHits(postId);
-        PostDTO post = convertPostDateFormat(boardMapper.getPost(postId));
-        mv.addObject("post", post);
-        mv.addObject("categoryList", categoryList);
-        mv.setViewName("view");
+        return postServiceUtil.convertPostDataFormat(boardMapper.getPost(postId));
+    }
+
+    /**
+     * /write get 요청을 처리하는 메소드
+     * @return 게시글 작성 viewName과 카테고리 리스트
+     */
+    public ModelAndView getWriteForm() {
+
+        ModelAndView mv = new ModelAndView("write-form");
+        mv.addObject("categoryList", boardMapper.getCategoryList());
 
         return mv;
     }
 
 
-
     /**
-     * 페이징에 필요한 변수값과 현재 페이지에 잘못된 값이 입력된 경우 이를 보정하는 메소드
-     * @param totalPostCount 전체 게시글 개수
-     * @param pageNumber 입력받은 요청 페이지 번호
-     * @return 페이징에 필요한 시작페이지와 끝 페이지 값과 보정된 요청 페이지 값, 총 페이지 값(끝으로 이동 시 사용)
+     * 비밀번호 암호화 및 생성시간을 추가해서 게시글을 저장하는 메소드
+     * @param post 저장할 게시글
      */
-    private HashMap<String, Integer> calPagingValues(int totalPostCount, Integer pageNumber) {
-        HashMap<String, Integer> values = new HashMap<>();
+    public void savePost(PostDTO post) {
 
-        int totalPage = totalPostCount / 10;
-        if (totalPostCount % 10 > 0) {
-            totalPage++;
+        if (!postServiceUtil.checkValidation(post)) {
+            throw new RuntimeException();
         }
 
-        int currentPage = pageNumber;
+        // TODO: 2/25. 단방향 암호화만 하고 복호화를 못하게 하는 이유?
+        post.setPasswd(postServiceUtil.getSHA512(post.getPasswd()));
+        post.setCreatedDate(String.valueOf(LocalDateTime.now()));
 
-        // 유효 페이지 이외의 값을 파라미터로 받게 되는 경우의 예외처리
-        if (currentPage > totalPage || currentPage <= 0) {
-            currentPage = 1;
-        }
-
-        int startPage = ((currentPage - 1) / 10) * 10 + 1;
-        int endPage = startPage + 10 - 1;
-
-        // 마지막 페이지에서 페이지 범위 조정
-        if (endPage > totalPage) {
-            endPage = totalPage;
-        }
-
-        values.put("startPage", startPage);
-        values.put("endPage", endPage);
-        values.put("currentPage", currentPage);
-        values.put("totalPage", totalPage);
-
-        return values;
-    }
-
-    /**
-     * 날짜 변환과 수정일이 공란일 시 "-"을 추가하고 각 포스트마다 파일 보유 여부를 추가하는 메소드
-     * @param postList 게시글 리스트
-     * @return 수정된 파라미터값(게시글 리스트)을 반환
-     */
-    private List<PostDTO> convertPostListData(List<PostDTO> postList) {
-
-        List<PostDTO> result = new LinkedList<>();
-
-        for (PostDTO post : postList) {
-
-            post = convertPostDateFormat(post);
-
-            // 제목 길이 수정
-            if (post.getTitle().length() > 80) {
-                post.setTitle(post.getTitle().substring(0, 80) + "...");
-            }
-
-            // 줄 띄어쓰기 적용
-            post.setContent(post.getContent().replaceAll("\r\n", "<br>"));
-
-            // 현재 게시글의 파일 여부 확인시 fileFlag 설정
-            // checkFileExistence 메소드는 file쪽 메소드인데 file에 놓지 않고 post에 놓아도 될까?(이 메소드를 이용해서 Post 데이터의 필드에 관여하는 기능이기 때문에 post에 놓았음)
-            long postId = post.getPostId();
-            if (boardMapper.checkFileExistence(postId)) {
-                post.setFileFlag(true);
-            }
-
-            result.add(post);
-        }
-        return result;
-    }
-
-    private PostDTO convertPostDateFormat(PostDTO post) {
-
-        String createdDate = post.getCreatedDate();
-        // 날짜 포맷 변경
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime ldt = LocalDateTime.parse(createdDate, format);
-
-        post.setCreatedDate(ldt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
-
-        if (post.getModifiedDate() == null) {
-            post.setModifiedDate("-");
-        } else {
-            String modifiedDate = post.getModifiedDate();
-            ldt = LocalDateTime.parse(modifiedDate, format);
-            post.setModifiedDate(ldt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")));
-        }
-
-        return post;
+        boardMapper.savePost(post);
     }
 }
